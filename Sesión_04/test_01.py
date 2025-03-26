8e982d2b91831769bf454c88ad5594ba2dffaa30
Cesar Galindo <astraeayiyi@gmail.com>
2025-03-25 23:06:23 -0500

    Nuevos Archivos

---
+++ Sesión_04/test_01.py
@@ -0,0 +1,18 @@
+#Diccionarios  en Python
+
+"""Los nombres de los key(llaves) van a ir escritos siempre en minúsculas y
+ sin tildes (por convención o buenas prácticas)"""
+
+var_1 ={"nombre":"Susana","edad":29,"promedio":14.9}
+
+print("Mi diccionario tiene el siguiente contenido: {} ".format(var_1))
+
+"""Agregamos elementos con un nuevo key a mi diccionario  var[] = "____" """
+
+var_1["distrito"] = "Lince"
+var_1["nombre"] = "César"
+
+
+print("Mi diccionario actualizado tiene la foram de : {} ".format(var_1))
+
+
